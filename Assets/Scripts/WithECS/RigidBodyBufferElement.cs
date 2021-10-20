5fff989bb483c203efa004dc77140c4f0cc849fe
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-10-20 21:33:20 +0700

    Remove old script

--- Assets/Scripts/WithECS/RigidBodyBufferElement.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-[InternalBufferCapacity(100)]
-public struct RigidBodyBufferElement : IBufferElementData
-{
-    public float3 position;
-    public float3 velocity;
-}
